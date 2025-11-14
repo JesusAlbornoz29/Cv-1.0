834c680fc0b2a25862d350ded42ee00520daf85f
JesusAlbornoz29 <alfredo.albornoz@estudiantes.utec.edu.uy>
2025-11-14 17:42:02 -0300

    Agregando proyecto de Turnos modificando el placeholder y projects-section

--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -1,32 +1,40 @@
 import { ProjectCard } from "@/components/project-card";
 import { PlaceHolderImages } from "@/lib/placeholder-images";
+import Image from "../../public/img/SistemaDeTurnos.png";
 
 const projects = [
+    {
+    title: "Sistema de turnos",
+    description: "Una aplicación web de gestión de turnos que permite a los usuarios solicitar citas médicas rápidamente, generando un número de cita único al instante y gestionando el llamado mediante el cambio de estados de los tickets de los pacientes.",
+    image: PlaceHolderImages[0],
+    liveUrl: "https://sistemadeturno.netlify.app",
+    codeUrl: "https://github.com/JesusAlbornoz29/SistemaDeTurnos.git",
+  },
   {
     title: "Gestor de Tareas",
     description: "Una aplicación web de gestión de tareas con un diseño moderno y funcionalidades intuitivas para organizar tu trabajo diario.",
-    image: PlaceHolderImages[0],
+    image: PlaceHolderImages[1],
     liveUrl: "#",
     codeUrl: "#",
   },
   {
     title: "Rastreador de Fitness",
     description: "Una aplicación móvil para seguir tus metas de fitness, registrar entrenamientos y visualizar tu progreso a lo largo del tiempo.",
-    image: PlaceHolderImages[1],
+    image: PlaceHolderImages[2],
     liveUrl: "#",
     codeUrl: "#",
   },
     {
     title: "Plataforma E-commerce",
     description: "Una plataforma de comercio electrónico construida para un negocio local, con un catálogo de productos, carrito de compras y pasarela de pago.",
-    image: PlaceHolderImages[2],
+    image: PlaceHolderImages[3],
     liveUrl: "#",
     codeUrl: "#",
   },
   {
     title: "Dashboard de Datos",
     description: "Un panel de visualización de datos que muestra las tendencias del mercado con gráficos y tablas interactivas.",
-    image: PlaceHolderImages[3],
+    image: PlaceHolderImages[4],
     liveUrl: "#",
     codeUrl: "#",
   },
